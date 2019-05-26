91a682eb9a08633299d72598c38d930cfd6417ff
jk977 <jdk64@comcast.net>
2019-05-25 19:01:20 -0700

    Making hello world ncurses program

--- src/main.c
+++ src/main.c
@@ -1,8 +1,40 @@
-#include <stdio.h>
-#include <curses.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
+#include <locale.h>
+#include <curses.h>
+#include <signal.h>
+
+void setup_curses(int argc, char** argv) {
+    /*
+     * initializes and configures curses window
+     */
+
     (void) argc;
     (void) argv;
+
+    initscr();
+    cbreak();
+    noecho();
+    intrflush(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+}
+
+void cleanup(void) {
+    /*
+     * tidying function invoked via atexit()
+     */
+
+    endwin();
+}
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "en_US.utf-8");
+    setup_curses(argc, argv);
+    atexit(cleanup);
+
+    addstr("hello, world!");
+
+    refresh();
+    getch();
     return 0;
 }
